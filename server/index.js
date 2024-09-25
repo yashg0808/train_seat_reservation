9570ba781321df9bd4ff047a2fbc6cf7c8669eb8
Yash Gupta <yash.gupta0808@gmail.com>
2024-09-25 19:30:14 +0530

    exported express app

--- server/index.js
+++ server/index.js
@@ -19,5 +19,4 @@ app.use(express.json());
 app.use("/api", seatRoutes);  // Prefix all routes with /api
 
 // Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
