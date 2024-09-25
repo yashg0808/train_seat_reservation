6852e540983045c4a8ad31944bb6c2da89790af9
Yash Gupta <yash.gupta0808@gmail.com>
2024-09-25 19:54:52 +0530

    backend uri

--- client/src/services/seatService.js
+++ client/src/services/seatService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://train-seat-reservation-backend.vercel.app/api";
 
 // Get all seats from the backend
 export const getSeats = async () => {
