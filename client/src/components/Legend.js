155699cb8423ca8487da193b451d357a506872b5
Yash Gupta <yash.gupta0808@gmail.com>
2024-09-26 12:03:16 +0530

    mongo transaction & booking description

--- client/src/components/Legend.js
+++ client/src/components/Legend.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const Legend = () => {
   return (
-    <div className="legend">
-      <div className="legend-item">
-        <div className="seat available"></div>
-        <span>Available</span>
+    <div>
+      <div className="legend">
+        <div className="legend-item">
+          <div className="seat available"></div>
+          <span>Available</span>
+        </div>
+        <div className="legend-item">
+          <div className="seat booked"></div>
+          <span>Booked</span>
+        </div>
       </div>
-      <div className="legend-item">
-        <div className="seat booked"></div>
-        <span>Booked</span>
+      <div className="legend">
+      <h3>How booking works:</h3>
+      <p>One person can reserve up to 7 seats at a time.</p>
+      <p>
+        If a person is reserving seats, the priority will be to book them in one
+        row
+      </p>
+      <p>
+        If seats are not available in one row then the booking will be done in
+        such a way that the nearby seats are booked.
+      </p>
       </div>
     </div>
   );
